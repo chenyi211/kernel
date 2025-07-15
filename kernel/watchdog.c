chenyi211/kernel

commit f61b11535a0bcb5c0a90f626a757cb710d71409c

watchdog: Support watchdog_sdei coexist with existing watchdogs
kunpeng inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I90N2C
CVE: NA

----------------------------------------------------------------------

Currently we cannot use watchdog_{perf, buddy} if CONFIG_SDEI_WATCHDOG=y.
Not all the platforms has watchdog_sdei so this patch tries to make
watchdog_sdei coexist with other watchdogs.

Only one watchdog will finally works. By default watchdog_sdei
will be used. If boot with "disable_sdei_nmi_watchdog", other
watchdogs will be used if probed.

Signed-off-by: Yicong Yang <yangyicong@hisilicon.com>
Signed-off-by: Jie Liu <liujie375@h-partners.com>

kernel/watchdog.c
@@ -562,8 +562,12 @@ static void watchdog_enable(unsigned int cpu)
 	/* Initialize timestamp */
 	update_touch_ts();
 	/* Enable the hardlockup detector */
-	if (watchdog_enabled & WATCHDOG_HARDLOCKUP_ENABLED)
-		watchdog_hardlockup_enable(cpu);
+	if (watchdog_enabled & WATCHDOG_HARDLOCKUP_ENABLED) {
+		if (disable_sdei_nmi_watchdog)
+			watchdog_hardlockup_enable(cpu);
+		else
+			sdei_watchdog_hardlockup_enable(cpu);
+	}
 }
 
 static void watchdog_disable(unsigned int cpu)
@@ -577,7 +581,10 @@ static void watchdog_disable(unsigned int cpu)
 	 * delay between disabling the timer and disabling the hardlockup
 	 * detector causes a false positive.
 	 */
-	watchdog_hardlockup_disable(cpu);
+	if (disable_sdei_nmi_watchdog)
+		watchdog_hardlockup_disable(cpu);
+	else
+		sdei_watchdog_hardlockup_disable(cpu);
 	hrtimer_cancel(hrtimer);
 	wait_for_completion(this_cpu_ptr(&softlockup_completion));
 }
@@ -1022,7 +1029,8 @@ void __init lockup_detector_init(void)
 	cpumask_copy(&watchdog_cpumask,
 		     housekeeping_cpumask(HK_TYPE_TIMER));
 
-	if (!watchdog_hardlockup_probe())
+	if ((!disable_sdei_nmi_watchdog && !sdei_watchdog_hardlockup_probe()) ||
+	    !watchdog_hardlockup_probe())
 		watchdog_hardlockup_available = true;
 	else
 		allow_lockup_detector_init_retry = true;

arch/arm64/kernel/watchdog_sdei.c
@@ -25,7 +25,7 @@ bool disable_sdei_nmi_watchdog;
 static bool sdei_watchdog_registered;
 static DEFINE_PER_CPU(ktime_t, last_check_time);
 
-void watchdog_hardlockup_enable(unsigned int cpu)
+void sdei_watchdog_hardlockup_enable(unsigned int cpu)
 {
 	int ret;
 
@@ -45,7 +45,7 @@ void watchdog_hardlockup_enable(unsigned int cpu)
 	}
 }
 
-void watchdog_hardlockup_disable(unsigned int cpu)
+void sdei_watchdog_hardlockup_disable(unsigned int cpu)
 {
 	int ret;
 
@@ -106,7 +106,7 @@ void sdei_watchdog_clear_eoi(void)
 		sdei_api_clear_eoi(SDEI_NMI_WATCHDOG_HWIRQ);
 }
 
-int __init watchdog_hardlockup_probe(void)
+int __init sdei_watchdog_hardlockup_probe(void)
 {
 	int ret;
 

include/linux/nmi.h
@@ -232,10 +232,16 @@ static inline void nmi_backtrace_stall_check(const struct cpumask *btp) {}
 #endif
 
 #ifdef CONFIG_SDEI_WATCHDOG
+void sdei_watchdog_hardlockup_enable(unsigned int cpu);
+void sdei_watchdog_hardlockup_disable(unsigned int cpu);
 void sdei_watchdog_clear_eoi(void);
+int sdei_watchdog_hardlockup_probe(void);
 extern bool disable_sdei_nmi_watchdog;
 #else
+static inline void sdei_watchdog_hardlockup_enable(unsigned int cpu) { }
+static inline void sdei_watchdog_hardlockup_disable(unsigned int cpu) { }
 static inline void sdei_watchdog_clear_eoi(void) { }
+static inline int sdei_watchdog_hardlockup_probe(void) { return -ENODEV; }
 #define disable_sdei_nmi_watchdog 1
 #endif
 

lib/Kconfig.debug
@@ -1117,7 +1117,6 @@ config HARDLOCKUP_DETECTOR_PERF
 	depends on HARDLOCKUP_DETECTOR
 	depends on HAVE_HARDLOCKUP_DETECTOR_PERF && !HARDLOCKUP_DETECTOR_PREFER_BUDDY
 	depends on !HAVE_HARDLOCKUP_DETECTOR_ARCH
-	depends on !SDEI_WATCHDOG
 	select HARDLOCKUP_DETECTOR_COUNTS_HRTIMER
 
 config HARDLOCKUP_DETECTOR_BUDDY
@@ -1126,7 +1125,6 @@ config HARDLOCKUP_DETECTOR_BUDDY
 	depends on HAVE_HARDLOCKUP_DETECTOR_BUDDY
 	depends on !HAVE_HARDLOCKUP_DETECTOR_PERF || HARDLOCKUP_DETECTOR_PREFER_BUDDY
 	depends on !HAVE_HARDLOCKUP_DETECTOR_ARCH
-	depends on !SDEI_WATCHDOG
 	select HARDLOCKUP_DETECTOR_COUNTS_HRTIMER
 
 config HARDLOCKUP_DETECTOR_ARCH

commit 4c7e604c02cded9610fc3cab63e8c1358a80c2ed

downstream: watchdog: Fix call trace when failed to initialize sdei
In the above commit, sdei_watchdog needs to move down
lockup_detector_init (), while nmi_watchdog does not. So when
sdei_watchdog fails to be initialized, nmi_watchdog should not be
initialized.

[    0.706631][    T1] SDEI NMI watchdog: Disable SDEI NMI Watchdog in VM
[    0.707405][    T1] ------------[ cut here ]------------
[    0.708020][    T1] WARNING: CPU: 0 PID: 1 at kernel/watchdog_perf.c:117 hardlockup_detector_event_create+0x24/0x108
[    0.709230][    T1] Modules linked in:
[    0.709665][    T1] CPU: 0 PID: 1 Comm: swapper/0 Not tainted 6.6.0 #1
[    0.710700][    T1] Hardware name: QEMU KVM Virtual Machine, BIOS 0.0.0 02/06/2015
[    0.711625][    T1] pstate: 00400005 (nzcv daif +PAN -UAO -TCO -DIT -SSBS BTYPE=--)
[    0.712547][    T1] pc : hardlockup_detector_event_create+0x24/0x108
[    0.713316][    T1] lr : watchdog_hardlockup_probe+0x28/0xa8
[    0.714010][    T1] sp : ffff8000831cbdc0
[    0.714501][    T1] pmr_save: 000000e0
[    0.714957][    T1] x29: ffff8000831cbdc0 x28: 0000000000000000 x27: 0000000000000000
[    0.715899][    T1] x26: 0000000000000000 x25: 0000000000000000 x24: 0000000000000000
[    0.716839][    T1] x23: 0000000000000000 x22: 0000000000000000 x21: ffff80008218fab0
[    0.717775][    T1] x20: ffff8000821af000 x19: ffff0000c0261900 x18: 0000000000000020
[    0.718713][    T1] x17: 00000000cb551c45 x16: ffff800082625e48 x15: ffffffffffffffff
[    0.719663][    T1] x14: 0000000000000000 x13: 205d315420202020 x12: 5b5d313336363037
[    0.720607][    T1] x11: 00000000ffff7fff x10: 00000000ffff7fff x9 : ffff800081b5f630
[    0.721590][    T1] x8 : 00000000000bffe8 x7 : c0000000ffff7fff x6 : 000000000005fff4
[    0.722528][    T1] x5 : 00000000002bffa8 x4 : 0000000000000000 x3 : 0000000000000000
[    0.723482][    T1] x2 : 0000000000000000 x1 : 0000000000000140 x0 : ffff0000c02c0000
[    0.724426][    T1] Call trace:
[    0.724808][    T1]  hardlockup_detector_event_create+0x24/0x108
[    0.725535][    T1]  watchdog_hardlockup_probe+0x28/0xa8
[    0.726174][    T1]  lockup_detector_init+0x110/0x158
[    0.726776][    T1]  kernel_init_freeable+0x208/0x288
[    0.727387][    T1]  kernel_init+0x2c/0x200
[    0.727902][    T1]  ret_from_fork+0x10/0x20
[    0.728420][    T1] ---[ end trace 0000000000000000 ]---

Fixes: f61b11535a0b ("watchdog: Support watchdog_sdei coexist with existing watchdogs")

kernel/watchdog.c
@@ -1030,7 +1030,7 @@ void __init lockup_detector_init(void)
 		     housekeeping_cpumask(HK_TYPE_TIMER));
 
 	if ((!disable_sdei_nmi_watchdog && !sdei_watchdog_hardlockup_probe()) ||
-	    !watchdog_hardlockup_probe())
+	    (disable_sdei_nmi_watchdog && !watchdog_hardlockup_probe()))
 		watchdog_hardlockup_available = true;
 	else
 		allow_lockup_detector_init_retry = true;